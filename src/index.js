58d3f9d76687f8fe14994620bccd3b6f681c19c5
Federico del Mazo <FdelMazo@fi.uba.ar>
2023-06-05 20:35:15 -0300

    Use reactv18 stuff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import MainApp from "./components/MainApp";
 import useData from "./useData";
 import { DataContext } from "./Contexts"
@@ -15,9 +15,11 @@ const App = () => {
   );
 };
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
   <ChakraProvider>
     <App />
-  </ChakraProvider>,
-  document.getElementById("root")
+  </ChakraProvider>
 );
