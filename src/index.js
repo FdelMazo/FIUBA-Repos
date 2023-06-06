39647b9e2ed364493e52dc0ca9fccfad03a19c7f
Federico del Mazo <FdelMazo@fi.uba.ar>
2023-06-05 21:18:58 -0300

    Revert "Use reactv18 stuff"
    This reverts commit 58d3f9d76687f8fe14994620bccd3b6f681c19c5.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import MainApp from "./components/MainApp";
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-
-root.render(
+ReactDOM.render(
   <ChakraProvider>
     <MainApp />
-  </ChakraProvider>
+  </ChakraProvider>,
+  document.getElementById("root")
 );
