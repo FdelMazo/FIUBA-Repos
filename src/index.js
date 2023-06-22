e5bc03ffeadf70e669f8cd6fe73a950d5fd218af
Federico del Mazo <FdelMazo@fi.uba.ar>
2023-06-22 16:10:24 -0300

    Upgrade to react18 + chakra2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import MainApp from "./components/MainApp";
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
   <ChakraProvider>
     <MainApp />
-  </ChakraProvider>,
-  document.getElementById("root")
+  </ChakraProvider>
 );
