eddb5ac34d8000890ba25ca3635923f1f2b74b3f
Juli <jarivero@fi.uba.ar>
2024-08-11 21:04:58 -0300

    formateo del json por separado

---
+++ src/data/obtener-subir-materias/formatear_json.py
@@ -0,0 +1,39 @@
+import os
+
+# Obtener la ruta absoluta del directorio actual del script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construir las rutas absolutas
+materias_a_subir_path = os.path.join(script_dir, "materias-a-subir.json")
+materias_path = os.path.abspath(os.path.join(script_dir, "../materias.json"))
+
+import json
+
+def format_name(texto: str):
+    words   = texto.split()
+    palabras_vacias = ['y','para', 'de', 'del', 'el', 'los', 'la', 'las', 'al', 'su', 'sus', 'en', 'a', 'por', 'no', 'e', 'con']
+    nivel = ['I','II','III','IV','V']
+    variantes = ['A','B','C']
+    excepciones = ['AT', 'SEP']
+    
+
+    for index, word in enumerate(words):
+        if not word in excepciones:
+            if index == len(words)-1 and word.upper() in variantes:
+                words[index] = word.upper()
+            elif word.upper() in nivel:
+                words[index] = word.upper()
+            elif word.lower() in palabras_vacias:
+                words[index] = word.lower()
+            else: 
+                words[index] = word.capitalize()
+    
+    return ' '.join(words)
+
+with open(materias_path, "r", encoding='utf-8') as archivo_materias:
+    materias = json.load(archivo_materias)
+    for key, value in materias.items():
+        materias[key] = format_name(value)
+
+with open(materias_path, "w", encoding='utf-8') as archivo_materias:
+    json.dump(materias, archivo_materias, ensure_ascii=False, indent=2)
--- src/data/obtener-subir-materias/obtenerMateriasdelSiuOfertaComisiones.js
+++ src/data/obtener-subir-materias/obtenerMateriasdelSiuOfertaComisiones.js
@@ -2,9 +2,6 @@
 
 const periodos = document.querySelectorAll("[periodo]");
 const data = new Map()
-const capitalizeFirstSentenceLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-const capitalizeFirstWordLetter = (string) => string.split(" ").map((word)=> capitalizeFirstSentenceLetter(word)).join(" ");
-
 
 periodos.forEach(periodo => {
   const materias = periodo.querySelectorAll("[actividad]")
@@ -12,7 +9,7 @@ periodos.forEach(periodo => {
     const indiceParentesis = materia.getAttribute("actividad").indexOf("(");
     const materiaId = materia.getAttribute("actividad").slice(indiceParentesis + 1, -1);
     const materiaName = materia.getAttribute("actividad").slice(0, indiceParentesis).trim();
-    data.set(materiaId, capitalizeFirstWordLetter(materiaName));
+    data.set(materiaId, materiaName);
   })
 })
 console.log(Object.fromEntries(data))
